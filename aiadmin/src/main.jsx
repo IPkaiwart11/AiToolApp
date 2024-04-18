7469faa8fb862b5d67ea23283f5eee584f0d141c
IPkaiwart11 <kaiwartindrapal@gmail.com>
2024-04-18 14:41:16 +0530

    initial commit

--- aiadmin/src/main.jsx
+++ aiadmin/src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 import './index.css'
 import SignUp from './pages/Signup/SignUp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-  // <React.StrictMode>
+
     {/* <App /> */}
 <SignUp/>
-  // </React.StrictMode>,
+ 
   </>
 )
--- aiadmin/vite.config.js
+++ aiadmin/vite.config.js
@@ -14,4 +14,4 @@ export default defineConfig({
     target: 'modules',
     outDir: 'dist',
   },
-})
+});
