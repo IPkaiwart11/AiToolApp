f2dac75bc16f21499231b57b9e7a50c272172dc7
IPkaiwart11 <kaiwartindrapal@gmail.com>
2024-05-18 01:07:06 +0530

    initial commit

--- aiadmin/src/App.jsx
+++ aiadmin/src/App.jsx
@@ -7,20 +7,20 @@ import {
 } from "react-router-dom";
 import Home from './pages/home/Home';
 import Newtool from './pages/newTool/NewTool';
-import SignIn from './pages/signin/SignIn'
-import SignUp from './pages/Signup/SignUp'
+import SignIn from './pages/signin/SignIn';
+import SignUp from './pages/Signup/SignUp';
 function App() {
 
   return (
     <>
-     <Router>
-    <Routes  path="/">
-      <Route index element={<Home/>}/>
-      <Route path="/sign-in" element={<SignIn/>}/>
-      <Route path="/sign-up" element={<SignUp/>}/>
-      <Route path="new"  element={<Newtool/>}/>
-    </Routes> 
-  </Router>
+      <Router>
+        <Routes >
+        <Route path="/"element={<Home />} /> 
+        <Route path="/sign-in" element={<SignIn/>}  /> 
+        <Route path="/sign-up" element={<SignUp/>}  /> 
+        <Route path="new" element={<Newtool />} />
+        </Routes>
+      </Router>
     </>
   )
 }
